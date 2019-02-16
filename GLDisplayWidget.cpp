0834e2bcea687af48cbd6b20e2d23165893772b6
miiicom <miiicom1230@gmail.com>
2019-02-16 15:45:40 -0700

    Fix normal and uv bugs

--- GLDisplayWidget.cpp
+++ GLDisplayWidget.cpp
@@ -277,7 +277,7 @@ void GLDisplayWidget::sendDataToOpenGL() {
 
 	std::vector<cyPoint3f> teapotNormals;
 	for (int i = 0; i < teapot.NV(); i++) {
-		teapotVertices.push_back(teapot.VN(i));
+		teapotNormals.push_back(teapot.VN(i));
 	}
 
 	//std::vector<cyPoint3f> teapotUVs;
@@ -288,9 +288,10 @@ void GLDisplayWidget::sendDataToOpenGL() {
 	std::vector<cyPoint3f> teapotUVs;
 	teapotUVs.resize(teapot.NV());
 	for (int i = 0; i < teapot.NF(); i++) {
-		teapotUVs[teapot.F(i).v[0]] = teapot.VT(teapot.FT(i).v[0]);
-		teapotUVs[teapot.F(i).v[1]] = teapot.VT(teapot.FT(i).v[1]);
-		teapotUVs[teapot.F(i).v[2]] = teapot.VT(teapot.FT(i).v[2]);
+		for (int j = 0; j < 3; j++) {
+			teapotUVs[teapot.F(i).v[j]] = cyPoint3f(teapot.VT(teapot.FT(i).v[j]).x,
+				teapot.VT(teapot.FT(i).v[j]).y, 0);
+		}
 	}
 
 	std::vector<cyPoint3f> teapotInfos;
@@ -311,7 +312,7 @@ void GLDisplayWidget::sendDataToOpenGL() {
 	// read vertex position info only
 	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, teapot.NF() * sizeof(unsigned int) *3 , &teapot.F(0), GL_STATIC_DRAW);
 	// read vertex information from a vector
-	glBufferData(GL_ARRAY_BUFFER, (teapot.NV() + teapot.NVN() + teapot.NV())* sizeof(cyPoint3f), &teapotVertices[0], GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, (teapot.NV() + teapot.NVN() + teapot.NV())* sizeof(cyPoint3f), &teapotInfos[0], GL_STATIC_DRAW);
 	
 
 	glGenBuffers(1, &teapotIndexBufferID);
@@ -344,7 +345,7 @@ void GLDisplayWidget::setupVertexArrays()
 	glBindBuffer(GL_ARRAY_BUFFER, teapotVertexBufferID);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*)(sizeof(cyPoint3f) * teapot.NV()));
-	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE,0, (void*)(sizeof(cyPoint3f) *  teapot.NVN()));
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, (void*)(sizeof(cyPoint3f) * teapot.NVN() + sizeof(cyPoint3f) * teapot.NV()));
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, teapotIndexBufferID);
 }
 //--------------Shader utility functions
