12ef739dca7ab95062ed8a1c4e0da7732c022388
miiicom <miiicom1230@gmail.com>
2019-02-20 09:47:11 -0700

    Add simple cubemap shader

---
+++ shaders/CubeMapFragmentShader.glsl
@@ -0,0 +1,21 @@
+#version 430
+
+in vec3 ReflectDir;  // The direction of the reflected ray
+in vec3 VertexPositionWorld;
+in vec3 NormalWorld;
+in vec2 fragmentUV;
+
+out vec4 FragmentColor;
+
+uniform vec3 pointLightPosition;
+uniform float pointLightIntensity;
+uniform vec3 ambientLightUniform;
+uniform vec3 cameraPositionWorld;
+
+uniform samplerCube CubeMapTexture
+
+void main()
+{
+	vec4 cubeMapColor = texture(CubeMapTexture,ReflectDir);
+	FragmentColor = cubeMapColor;
+}
